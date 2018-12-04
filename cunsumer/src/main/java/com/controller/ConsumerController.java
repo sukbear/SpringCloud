6fa0472ee76e3c329324ed8a566d9f599b61e0c0
liufa <704759810@qq.com>
2018-12-04 15:17:04 +0800

    df

---
+++ cunsumer/src/main/java/com/controller/ConsumerController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.remote.HelloRemote;
+
+@RestController
+public class ConsumerController {
+	@Autowired
+	HelloRemote helloRemote;
+
+	@RequestMapping("/hello/{name}")
+	public String index(@PathVariable("name") String name) {
+		return helloRemote.hello(name);
+	}
+}
---
+++ cunsumer/src/main/java/com/remote/HelloRemote.java
@@ -0,0 +1,12 @@
+package com.remote;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+	
+	@RequestMapping(value = "/hello")
+	public String hello(@RequestParam(value = "name") String name);
+}
